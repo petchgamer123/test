a234c0f1c82a72f32cca3149ffcd8d7c26c1951e
petchgamer123 <69622369+petchgamer123@users.noreply.github.com>
2023-06-29 13:38:03 +0700

    Update routes.py

--- routes.py
+++ routes.py
@@ -1,77 +1,87 @@
-from fastapi import APIRouter, FastAPI, Depends, HTTPException, status, Request
-from fastapi.responses import RedirectResponse
-from google_auth_oauthlib.flow import Flow
-from starlette.middleware.sessions import SessionMiddleware
-from starlette.middleware import Middleware
-from starlette.requests import Request as StarletteRequest
-from google.oauth2 import id_token
-from google.auth.transport import requests
 import os
-from pathlib import Path
+import pathlib
 
-app = FastAPI()
+import requests
+from flask import Flask, session, abort, redirect, request
+from google.oauth2 import id_token
+from google_auth_oauthlib.flow import Flow
+from pip._vendor import cachecontrol
+import google.auth.transport.requests
 
-Router = APIRouter()
-app.add_middleware(SessionMiddleware, secret_key="dVu9jfC1PPVGRkq-X5nKaP_vDHC63CxQ2K4W0QVpFJo", session_cookie="user_session")
+app = Flask("Google Login App")
+app.secret_key = "CodeSpecialist.com"
 
-client_secrets_file = "client_secret.json"
+os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 
 GOOGLE_CLIENT_ID = "642643535438-mm2947mq2360qr4429tmcjec7lje530j.apps.googleusercontent.com"
+client_secrets_file = os.path.join(
+    pathlib.Path(__file__).parent, "client_secret.json")
+
 flow = Flow.from_client_secrets_file(
     client_secrets_file=client_secrets_file,
-    scopes=["openid", "email", "profile"],
-    redirect_uri="http://localhost:4200/auth/callback"
+    scopes=["https://www.googleapis.com/auth/userinfo.profile",
+            "https://www.googleapis.com/auth/userinfo.email", "openid"],
+    redirect_uri="http://localhost:4200/google/callback"
 )
 
-def login_is_required(func):
-    @wraps(func)
-    async def wrapper(request: Request, *args, **kwargs):
-        if "google_id" not in request.session:
-            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Login required")
-        return await func(request, *args, **kwargs)
+
+def login_is_required(function):
+    def wrapper(*args, **kwargs):
+        if "google_id" not in session:
+            return abort(401)  # Authorization required
+        else:
+            return function()
+
     return wrapper
 
-@app.get("/google/login")
-async def login(request: StarletteRequest):
+
+@app.route("/google/login")
+def login():
     authorization_url, state = flow.authorization_url()
-    request.session["state"] = state
-    return RedirectResponse(authorization_url)
+    session["state"] = state
+    return redirect(authorization_url)
 
-@app.get("/auth/callback")
-async def callback(request: StarletteRequest):
-    flow.fetch_token(authorization_response=str(request.url))
 
-    if not request.session["state"] == request.query_params["state"]:
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="State does not match!")
+@app.route("/google/callback")
+def callback():
+    flow.fetch_token(authorization_response=request.url)
+
+    if not session["state"] == request.args["state"]:
+        abort(500)  # State does not match!
 
     credentials = flow.credentials
-    request_session = requests.RequestsSession()
-    cached_session = requests.session.CachedSession(request_session)
-    token_request = requests.Request(session=cached_session)
+    request_session = requests.session()
+    cached_session = cachecontrol.CacheControl(request_session)
+    token_request = google.auth.transport.requests.Request(
+        session=cached_session)
 
     id_info = id_token.verify_oauth2_token(
-        id_token=credentials.id_token,
+        id_token=credentials._id_token,
         request=token_request,
         audience=GOOGLE_CLIENT_ID
     )
 
-    request.session["google_id"] = id_info.get("sub")
-    request.session["name"] = id_info.get("name")
-    console.log(id_info)
-    return RedirectResponse("/")
+    session["google_id"] = id_info.get("sub")
+    session["name"] = id_info.get("name")
+    return redirect("/")
 
 
-# @app.get("/logout")
-# async def logout(request: StarletteRequest):
-#     request.session.clear()
-#     return RedirectResponse("/")
-
-# @app.get("/protected_area")
-# async def protected_area(request: StarletteRequest):
-#     return f"Hello {request.session['name']}! <br/> <a href='/logout'><button>Logout</button></a>"
+# @app.route("/logout")
+# def logout():
+#     session.clear()
+#     return redirect("/")
 
 
-# @app.get("/")
-# async def index():
-#     return "Hello World <a href='/login'><button>Login</button></a>"
+# @app.route("/")
+# def index():
+#     return "Hello World <a href='/google/login'><button>Login</button></a>"
 
+
+# @app.route("/protected_area")
+# @login_is_required
+# def protected_area():
+#     return f"Hello {session['name']}! <br/> <a href='/logout'><button>Logout</button></a>"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
