033c681754fdb9dfea690c48b9610786c223c1ed
petchgamer123 <69622369+petchgamer123@users.noreply.github.com>
2023-06-29 16:59:39 +0700

    Update db.py

--- db.py
+++ db.py
@@ -1,5 +1,5 @@
 from pymongo import MongoClient
-from typing import Optional
+
 
 from pymongo import MongoClient
 db_connection = MongoClient("mongodb+srv://boat:1234@cluster0.rmsa1et.mongodb.net/")
