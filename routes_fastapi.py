724435d903052e389193f13267f02cd4c2535693
petchgamer123 <69622369+petchgamer123@users.noreply.github.com>
2023-06-30 14:14:00 +0700

    Update routes_fastapi.py

--- routes_fastapi.py
+++ routes_fastapi.py
@@ -65,7 +65,7 @@ async def callback(request: StarletteRequest):
     if(collection_account.find_one({"sub": id_info.get("sub")}) is None):
         user = collection_account.insert_one(id_info)
 
-    return RedirectResponse("localhost:4200/")
+    return RedirectResponse("https://www.google.com/")
 
 
 # @app.get("/logout")
